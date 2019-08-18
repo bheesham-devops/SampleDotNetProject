9b5eec05775da02504c0806d5d00225241949c7a
bheesham-devops <bheeshamkumar@gmail.com>
2019-08-18 12:46:51 +0530

    Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AWSSimpleApplication.Startup))]
+namespace AWSSimpleApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
